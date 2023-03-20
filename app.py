3237e644d1936b74a05ca628251884eb9da3f533
Artur C. Miranda <artcamir12@gmail.com>
2023-03-19 23:41:03 -0300

    Added a loop that prevents the user to type more than 1 word and also a cool ASCII animation when lost

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os, random
+import os, random, time
 from termcolor import colored
 from unidecode import unidecode
 
@@ -34,6 +34,10 @@ while (tentativasnum >= 1) :
     print(" ")
     letraTentada = input("Digite o palpite de letra: ").upper()
     tentativas.append(letraTentada)
+
+    while (len(letraTentada) != 1) :
+        print(colored("Você deve digitar apenas uma letra", "red")) 
+        letraTentada = input("Digite o palpite de letra: ").upper()
 
     if letraTentada in palavraSecretaList :
         for i, letra in enumerate(palavraSecretaList) :
@@ -73,7 +77,29 @@ while (tentativasnum >= 1) :
 
 
 if '_' in palavraSecretaCheck:
+    print(" _________")   # imprime a linha superior do enforcado
+    time.sleep(0.5)
+
+    print(" |         |")  # imprime a primeira linha vertical do enforcado
+    time.sleep(0.5)
+
+    print(" |         O")  # imprime a cabeça do enforcado
+    time.sleep(0.5)
+
+    print(" |        /|\\") # imprime os braços do enforcado
+    time.sleep(0.5)
+
+    print(" |        / \\") # imprime as pernas do enforcado
+    time.sleep(0.5)
+
+    print("_|_")           # imprime a base do enforcado
+    time.sleep(3)
+
+    clear()
+
     print(colored("Suas tentativas acabaram, tente novamente.", "yellow"))
+
+
 
 if (tentativasnum < 1) :
     clear()
